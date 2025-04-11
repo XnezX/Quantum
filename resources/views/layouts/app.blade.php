81f1533198229a248435b82c17a9963385401583
Nestor Molina <danielma1507@gmail.com>
2025-04-11 13:34:50 -0600

    Agrega funcionalidad de testimonios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SEPRINAL - @yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 </head>
-<body class="bg-gray-50">
+
     <div class="container mx-auto px-4 py-8">
         @yield('content')
     </div>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.export = {
+    darkMode: 'class', // Esto es el dark mode!
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}

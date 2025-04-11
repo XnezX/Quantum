c621d81e7d4a2f43db78bbce2abc2ce46c0a01db
Nestor Molina <danielma1507@gmail.com>
2025-04-11 17:55:51 -0600

    Nuevos cambios

--- resources/views/testimonios/index.blade.php
+++ resources/views/testimonios/index.blade.php
@@ -14,7 +14,7 @@
             <span class="sr-only">Use setting</span>
             <span class="relative inline-block w-5 h-5 transition duration-500 ease-in-out transform translate-x-0 bg-white rounded-full shadow pointer-events-none dark:translate-x-5 ring-0">
                 <span class="absolute inset-0 flex items-center justify-center w-full h-full transition-opacity duration-500 ease-in opacity-100 dark:opacity-0 dark:duration-100 dark:ease-out" aria-hidden="true">
-                    
+
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun w-4 h-4 text-neutral-700" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
@@ -22,7 +22,7 @@
                     </svg>
                 </span>
                 <span class="absolute inset-0 flex items-center justify-center w-full h-full transition-opacity duration-100 ease-out opacity-0 dark:opacity-100 dark:duration-200 dark:ease-in" aria-hidden="true">
-                    
+
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon w-4 h-4 text-neutral-700" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
@@ -33,7 +33,7 @@
     </div>
 
     {{-- Título  --}}
-    <h1 class="text-3xl font-bold text-center mb-10 text-gray-800 dark:text-gray-200">Testimonios</h1>
+    <h1 class="text-3xl font-bold text-center mb-10 text-gray-800 dark:text-gray-200">Sección Testimonios</h1>
 
     <div class="space-y-12">
         {{-- Sección de Cards de Testimonios --}}
@@ -47,33 +47,48 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
                 {{-- Card 1 --}}
                 <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                    <div class="h-full flex flex-col">
-                        <p class="text-lg mb-4 flex-grow dark:text-gray-300">SEPRINAL y su tecnología innovadora ha transformado por completo la forma en que operamos.</p>
-                        <div class="mt-auto">
-                            <p class="font-bold text-right dark:text-white">Felix Rojas</p>
-                            <p class="text-gray-600 dark:text-gray-400 text-right">Director de Seguridad IPS</p>
+                    <div class="h-full flex flex-col items-center">
+                        {{-- Imagen del cliente --}}
+                        <div class="w-24 h-24 mb-4 rounded-full overflow-hidden border-4 border-gray-500">
+                            <img src="/image/felixRojas.png" alt="Felix Rojas" class="w-full h-full object-cover">
+                        </div>
+
+                        <p class="text-lg mb-4 flex-grow dark:text-gray-300 text-center">SEPRINAL y su tecnología innovadora ha transformado por completo la forma en que operamos.</p>
+                        <div class="mt-auto w-full">
+                            <p class="font-bold text-center dark:text-white">Felix Rojas</p>
+                            <p class="text-gray-600 dark:text-gray-400 text-center">Director de Seguridad IPS</p>
                         </div>
                     </div>
                 </div>
 
                 {{-- Card 2 --}}
                 <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                    <div class="h-full flex flex-col">
-                        <p class="text-lg mb-4 flex-grow dark:text-gray-300">Cada solución que nos han proporcionado SEPRINAL ha superado nuestras expectativas, brindándonos resultados duraderos y de alto rendimiento.</p>
-                        <div class="mt-auto">
-                            <p class="font-bold text-right dark:text-white">Aurora Pulido</p>
-                            <p class="text-gray-600 dark:text-gray-400 text-right">Gerente de Administración La Costeña</p>
+                    <div class="h-full flex flex-col items-center">
+                        {{-- Imagen del cliente --}}
+                        <div class="w-24 h-24 mb-4 rounded-full overflow-hidden border-4 border-gray-500">
+                            <img src="/image/auroraPulido.png" alt="Aurora Pulido" class="w-full h-full object-cover">
+                        </div>
+
+                        <p class="text-lg mb-4 flex-grow dark:text-gray-300 text-center">Cada solución que nos han proporcionado SEPRINAL ha superado nuestras expectativas, brindándonos resultados duraderos y de alto rendimiento.</p>
+                        <div class="mt-auto w-full">
+                            <p class="font-bold text-center dark:text-white">Aurora Pulido</p>
+                            <p class="text-gray-600 dark:text-gray-400 text-center">Gerente de Administración La Costeña</p>
                         </div>
                     </div>
                 </div>
 
                 {{-- Card 3 --}}
                 <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                    <div class="h-full flex flex-col">
-                        <p class="text-lg mb-4 flex-grow dark:text-gray-300">Es un placer contar con un proveedor que pone tanto énfasis en la excelencia y la fiabilidad de lo que ofrece.</p>
-                        <div class="mt-auto">
-                            <p class="font-bold text-right dark:text-white">Rodrigo Camacho</p>
-                            <p class="text-gray-600 dark:text-gray-400 text-right">Gerente de T.I. SCS</p>
+                    <div class="h-full flex flex-col items-center">
+                        {{-- Imagen del cliente --}}
+                        <div class="w-24 h-24 mb-4 rounded-full overflow-hidden border-4 border-gray-500">
+                            <img src="/image/rodrigoCamacho.png" alt="Rodrigo Camacho" class="w-full h-full object-cover">
+                        </div>
+
+                        <p class="text-lg mb-4 flex-grow dark:text-gray-300 text-center">Es un placer contar con un proveedor que pone tanto énfasis en la excelencia y la fiabilidad de lo que ofrece.</p>
+                        <div class="mt-auto w-full">
+                            <p class="font-bold text-center dark:text-white">Rodrigo Camacho</p>
+                            <p class="text-gray-600 dark:text-gray-400 text-center">Gerente de T.I. SCS</p>
                         </div>
                     </div>
                 </div>
@@ -89,7 +104,7 @@
             <div class="text-center text-gray-500 my-6">***************</div>
 
             {{-- Sección con imagen de fondo --}}
-            <div class="relative bg-cover bg-center py-16 my-12 rounded-lg overflow-hidden" style="background-image: url('/image/background1.JPG'); min-height: 500px;">
+            <div class="relative bg-cover bg-center py-16 my-12 rounded-lg overflow-hidden " style="background-image: url('/image/background1.JPG'); min-height: 500px;">
                 {{-- Caja del contenido --}}
                 <div class="relative container mx-auto px-4 h-full">
                     {{-- Panel transparente --}}
